077a1141c1e8ffc1d31397de03e0def3353fff07
gilzzbotz <104905739+gilzzbotz@users.noreply.github.com>
2022-11-09 14:28:40 +0700

    Update menunya.js

--- src/menunya.js
+++ src/menunya.js
@@ -22,27 +22,27 @@ const menunya = `
 ╰━━•›ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙‹•━━͙✩̣̣̣̣
  ▬▭▬▭▬ ✦✧✦ ▬▭▬▭▬
 ╭━━•›〘 ALL MENU 〙
-𖠀⩵꙰ཱི࿐ BUGMENU 
-𖠀⩵꙰ཱི࿐ FUNMENU 
-𖠀⩵꙰ཱི࿐ MAINMENU 
-𖠀⩵꙰ཱི࿐ VOICEMENU 
-𖠀⩵꙰ཱི࿐ GROUPMENU 
-𖠀⩵꙰ཱི࿐ OWNERMENU 
-𖠀⩵꙰ཱི࿐ ISLAMICMENU 
-𖠀⩵꙰ཱི࿐ SEARCHMENU 
-𖠀⩵꙰ཱི࿐ EPHOTOMENU 
-𖠀⩵꙰ཱི࿐ RANDOMMENU 
-𖠀⩵꙰ཱི࿐ TEKSPROMENU 
-𖠀⩵꙰ཱི࿐ PRIMBONMENU 
-𖠀⩵꙰ཱི࿐ CONVERTMENU 
-𖠀⩵꙰ཱི࿐ WEBZONEMENU
-𖠀⩵꙰ཱི࿐ DATABASEMENU 
-𖠀⩵꙰ཱི࿐ PHOTOOXYMENU
-𖠀⩵꙰ཱི࿐ DOWNLOADMENU
-𖠀⩵꙰ཱི࿐ ANONYMOUSMENU
-╰━ ━ ━ ━ ━ ━ ━ ━ ━ ━•⩵꙰ཱི࿐
+😈 BUGMENU 
+😈 FUNMENU 
+😈 MAINMENU 
+😈 VOICEMENU 
+😈 GROUPMENU 
+😈 OWNERMENU 
+😈 ISLAMICMENU 
+😈 SEARCHMENU 
+😈 EPHOTOMENU 
+😈 RANDOMMENU 
+😈 TEKSPROMENU 
+😈 PRIMBONMENU 
+😈 CONVERTMENU 
+😈 WEBZONEMENU
+😈 DATABASEMENU 
+😈 PHOTOOXYMENU
+😈 DOWNLOADMENU
+😈 ANONYMOUSMENU
+╰━ ━ ━ ━ ━ ━ ━ ━ ━ ━•🗿
  ▬▭▬▭▬ ✦✧✦ ▬▭▬▭▬
 
-© ⏤͟͟͞𝑻𝑯𝑬 𝑱𝑶 𝑩𝑶𝑻𝒁
+© ⏤͟͟͞𝐆𝐢𝐥𝐳𝐳 𝐄𝐱𝐞
 `
 exports.menunya = menunya
